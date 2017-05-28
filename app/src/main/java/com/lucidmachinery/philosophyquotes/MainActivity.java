2d2658cbbba1a454126099ae547e016e0b384153
lucidmachine <lucid.machine@gmail.com>
2017-05-28 17:28:49 -0400

    Remove unused import.

--- app/src/main/java/com/lucidmachinery/philosophyquotes/MainActivity.java
+++ app/src/main/java/com/lucidmachinery/philosophyquotes/MainActivity.java
@@ -5,7 +5,6 @@ import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.Menu;
 import android.view.MenuItem;
-import android.widget.ArrayAdapter;
 import android.widget.ListView;
 
 import java.util.ArrayList;
