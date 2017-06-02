fd7aa89f79d434b2c26af25120918f19a622fa27
lucidmachine <lucid.machine@gmail.com>
2017-06-02 19:44:23 -0400

    Set onClick handler for publication link buttons.

--- app/src/main/java/com/lucidmachinery/philosophyquotes/QuotesAdapter.java
+++ app/src/main/java/com/lucidmachinery/philosophyquotes/QuotesAdapter.java
@@ -1,12 +1,15 @@
 package com.lucidmachinery.philosophyquotes;
 
 import android.content.Context;
+import android.content.Intent;
+import android.net.Uri;
 import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ArrayAdapter;
+import android.widget.ImageView;
 import android.widget.TextView;
 
 import java.util.ArrayList;
@@ -38,15 +41,33 @@ class QuotesAdapter extends ArrayAdapter<Quote> {
         if (convertView == null) {
             convertView = LayoutInflater.from(getContext()).inflate(R.layout.quote, parent, false);
         }
+        final View finalConvertView = convertView;
+
+        // Get child views
         TextView tvQuoteAuthor = (TextView) convertView.findViewById(R.id.quote_author);
         TextView tvQuoteText = (TextView) convertView.findViewById(R.id.quote_text);
+        ImageView publicationButtonView = (ImageView) convertView.findViewById(R.id.source_button);
 
         // Get data
-        Quote quote = getItem(position);
+        final Quote quote = getItem(position);
 
         // Put data in view
         tvQuoteAuthor.setText(quote != null ? quote.authorName != null ? quote.authorName: "" : "");
         tvQuoteText.setText(quote != null ? quote.text != null ? quote.text : "" : "");
+        publicationButtonView.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if (quote != null
+                        && quote.publicationLink != null
+                        && !quote.publicationLink.isEmpty()) {
+                    Intent intent = new Intent();
+                    intent.setAction(Intent.ACTION_VIEW);
+                    intent.addCategory(Intent.CATEGORY_BROWSABLE);
+                    intent.setData(Uri.parse(quote.publicationLink));
+                    finalConvertView.getContext().startActivity(intent);
+                }
+            }
+        });
 
         return convertView;
     }
